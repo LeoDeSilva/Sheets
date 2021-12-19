46d44c8e1478b1400cb0047a19b55782d748f309
leoDesilva <46300158+leoDesilva@users.noreply.github.com>
2021-12-19 16:26:32 +0000

    overlay

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+// ===== Navigation =====
+
 function openSidebar() {
-	//	document.getElementById("mySidebar").style.width = "25%";
-	//document.getElementById("sidebar").classList.remove("hide");
 	document.getElementById("sidebar").style.width = "400px";
 	document.getElementById("open-sidebar").classList.add("hide");
 }
 function closeSidebar() {
-	//document.getElementById("sidebar").classList.add("hide");
 	document.getElementById("sidebar").style.width = "0";
 	document.getElementById("open-sidebar").classList.remove("hide");
 }
+
+function closeOverlay() {
+	document.getElementById("overlay-container").classList.add("hide");
+	document.getElementById("body-container").classList.remove("blur");
+}
+
+function openOverlay() {
+	document.getElementById("overlay-container").classList.remove("hide");
+	document.getElementById("body-container").classList.add("blur");
+}
+
+function submitOverlay() {
+	console.log("SUBMIT");
+	closeOverlay();
+}
+
+document.getElementById("overlay").addEventListener("click", (e) => {
+	e.stopPropagation();
+});
